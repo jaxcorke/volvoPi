3dac235e27b98f5da6663f8ee292c1b33fc32f14
jaxcorke <jaxx.cork@gmail.com>
2025-05-07 19:18:01 -0500

    fix temp

--- src/gui.py
+++ src/gui.py
@@ -492,7 +492,7 @@ class MainFrame(customtkinter.CTkFrame):
 
                 def upddate_temp(self):
                     if os_name == "linux":
-                        self.temp_value.configure(text=str(psutil.sensors_temperatures()[0]))
+                        self.temp_value.configure(text= str(psutil.sensors_temperatures()["cpu_thermal"[0].current])) + "C"
                     else:
                         self.temp_value.configure(text="N/A")
 
